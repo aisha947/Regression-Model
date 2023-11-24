36a60ba9e6fd3c2c20d6a805b85016702aabb2fb
Ayesha Ameen <Ayesha Ameen>
2023-11-24 14:33:49 +0500

    MAde changes, run the code

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
-FROM python:3.9
+FROM python:3.9-slim
 
+# Set the working directory
 WORKDIR /app
 
-COPY requirements.txt .
+# Copy the current directory contents into the container
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+# Make port 5000 available to the world outside this container
+EXPOSE 5000
 
-CMD ["python", "app.py"]
+# Define environment variable
+ENV NAME World
+
+# Run app.py when the container launches
+CMD ["gunicorn", "app:app", "-b", "0.0.0.0:5000"]
+
--- app.py
+++ app.py
@@ -1,40 +1,29 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from flask import Flask, request, jsonify
 
 # Read train data
 train_data = pd.read_csv('trainRegression.csv')
 # Convert train data to Numpy Array
 train_numpydata = train_data.to_numpy()
 
-from sklearn.linear_model import LinearRegression
-
-# Initialize the linear regression model
+# Initialize the linear regression model and fit it with the training data
 model = LinearRegression()
-
-# Prepare the train data
 X_train = train_numpydata[:, 0].reshape(-1, 1)  # Assuming 'X' is the feature column
 y_train = train_numpydata[:, 1]  # Assuming 'R' is the target column
-
-# Fit the model
 model.fit(X_train, y_train)
 
-# Display the coefficients
-print(f'Coefficient: {model.coef_}')
-print(f'Intercept: {model.intercept_}')
-
-from flask import Flask, request, jsonify
-
 app = Flask(__name__)
-
-# Load the trained model
-model = LinearRegression()
-model.coef_ = 2.0  # Replace with the actual coefficient
-model.intercept_ = 1.0  # Replace with the actual intercept
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Get the input data from the request
-        input_data = request.json['X']  # Assuming the input is a single value
+        input_data = request.json.get('X')  # Assuming the input is a single value
+
+        # Validate input data
+        if input_data is None:
+            raise ValueError("Input data 'X' is missing in the request.")
 
         # Make prediction
         prediction = model.predict([[input_data]])
@@ -42,8 +31,10 @@ def predict():
         # Return the prediction as JSON
         return jsonify({'prediction': prediction[0]})
     except Exception as e:
-        return jsonify({'error': str(e)})
+        # Log the exception details for debugging
+        print(f"Exception: {str(e)}")
+        return jsonify({'error': 'An error occurred while processing the request.'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, port=5002)  # Set the port explicitly
 
